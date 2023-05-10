52ea34812fd5bf535364eb59eb0de34d2f2c1500
stalkert <taranenko.yura@gmail.com>
2023-05-10 16:29:09 +0200

    login 4

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './app';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ToastContainer />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
